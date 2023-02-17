8a1f0df74834f057192d9c2ddf133ddea3befaa6
Alex Jarabi <jarabialex@gmail.com>
2023-02-17 14:08:36 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	char l;
+
+	for (d = 0; d < 10; d++)
+		putchar((d % 10) + '0');
+
+	for (l = 'a'; l <= 'f'; letter++)
+		putchar(letter)
+
+	putchar('\n');
+
+	return (0);
+}

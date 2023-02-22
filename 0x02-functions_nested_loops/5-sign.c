7cb40de2d0994405eac445bb22a20795d4040159
Alex Jarabi <jarabialex@gmail.com>
2023-02-22 06:20:52 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,27 +5,26 @@
  * @n: The number to check
  *
  * Return: 1 and print + if n is greater than zero
- * Return: 0 and print 0 if n is zero
- * Return: -1 and prints - if n is less than zero
+ * 0 and print 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
 	int value;
-	char sign = n == 0 ? '0' : n > 0 ? '+' : '-';
 
-	switch (sign)
+	if (n > 0)
 	{
-		case '+':
-			value = 1;
-			break;
-		case '0':
-			value = 0;
-			break;
-		case '-':
-			value = -1;
-			break;
+		value = 1;
+		_putchar('+');
+	} else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	} else
+	{
+		value = -1;
+		_putchar('-');
 	}
 
-	_putchar(sign);
 	return (value);
 }

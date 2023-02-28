e127c2d2796b1ec24f63d1a1df9e304f231a7b32
Alex Jarabi <jarabialex@gmail.com>
2023-02-28 13:45:42 -0800

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,14 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 
 	/* Copy individual characters from src to dest */
-	while (*(src + i) != '\n')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
 
 	/* Append null byte to end of dest */
-	*(dest + i) = '\n';
+	*(dest + i) = '\0';
 
 	return (dest);
 }

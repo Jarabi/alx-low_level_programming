1bf82a08741d1b6fc90cbdaececd81ef10e9583b
Alex Jarabi <jarabialex@gmail.com>
2023-03-01 11:52:46 -0800

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * *_strncat - function that concatenates two strings.
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: bytes to copy
+ *
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dest_len = 0;
+
+	/* Get the length of dest */
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	/* Copy src to dest */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	/* Append null character to end of dest */
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}

df663171186eec453e9d4f9da269e19272e11116
Alex Jarabi <jarabialex@gmail.com>
2023-03-02 02:31:13 -0800

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * *string_toupper - function to change lowercase letters to uppercase.
+ * @s: string to convert
+ *
+ * Return: str
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}

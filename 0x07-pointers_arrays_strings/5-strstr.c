a5b6652b725ff6915d8059f55442b68f7af919dc
Alex Jarabi <jarabialex@gmail.com>
2023-03-06 12:46:00 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, start, nlen;
+
+	start = 0;
+	nlen = 0;
+
+	while (needle[nlen] != '\0')
+		nlen++;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; j < nlen && haystack[i] == needle[j]; j++)
+		{
+			if (j == 0)
+				start = i;
+			if (j == nlen - 1)
+				return (haystack + start);
+			i++;
+		}
+	}
+	return (0);
+}

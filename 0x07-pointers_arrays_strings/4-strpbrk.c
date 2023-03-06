9140b15885dd347e615b25faf03f6cf51b5dd47e
Alex Jarabi <jarabialex@gmail.com>
2023-03-06 12:29:53 -0800

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+/**
+ * *_strpbrk - function that searches
+ * @s: string segment
+ * @accept: bytes
+ *
+ * Return: int
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (0);
+}

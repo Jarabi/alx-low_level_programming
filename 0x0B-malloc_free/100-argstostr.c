fd89ff98b9880ff9afbc90018041c3e10711b831
Alex Jarabi <jarabialex@gmail.com>
2023-03-14 13:46:06 -0700

    5. A function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,7 @@
 #include "main.h"
+#include "_strlen.c"
+#include "_strcpy.c"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * argstostr - a function that concatenates all the arguments of your program.
@@ -11,19 +12,28 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
+	int i, chars_len = 0, idx = 0;
 	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	s = (char *) malloc(ac * sizeof(char));
+	/* get total length of all args */
+	/* including the \n character */
+	for (i = 0; i < ac; i++)
+		chars_len += _strlen(av[i]) + 1;
+
+	s = (char *) malloc(chars_len * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < (ac - 1); i++)
-		s[i] = *av[i + 1];
+	for (i = 0; i < ac; i++)
+	{
+		_strcpy(s + idx, av[i]);
+		idx += _strlen(av[i]);
+		s[idx++] = '\n';
+	}
 
 	return (s);
 }
---
+++ 0x0B-malloc_free/_strcpy.c
@@ -0,0 +1,24 @@
+/**
+ * *_strcpy - Function that copies the string pointed to by src,
+ * including the null byte (\0), to the buffer pointer by dest.
+ * @dest: destination pointer
+ * @src: source pointer
+ *
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int i = 0;
+
+        /* Copy individual characters from src to dest */
+        while (*(src + i) != '\0')
+        {
+                *(dest + i) = *(src + i);
+                i++;
+        }
+
+        /* Append null byte to end of dest */
+        *(dest + i) = '\0';
+
+        return (dest);
+}

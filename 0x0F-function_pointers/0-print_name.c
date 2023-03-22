c4cecfaf1e77789e365202854aae426c7ff337ee
Alex Jarabi <jarabialex@gmail.com>
2023-03-22 14:41:10 -0700

    A function that prints a name.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - a function that prints a name
+ * @name: name of the person
+ * @f: function pointer
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

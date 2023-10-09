f5d89cee716f32cb946911ae312cdd42b27f1315
Alex Jarabi <jarabialex@gmail.com>
2023-10-09 20:13:27 +0300

    a program that finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,70 @@
 #include <stdio.h>
 
 /**
- * main - Starting point
+ * numLen - Returns the number of digits in a number
+ * @n: the number
  *
+ * Return: number of digits
+ */
+int numLen(int n)
+{
+	int len = 0;
+
+	if (!n)
+		return (1);
+
+	while (n)
+	{
+		n /= 10;
+		len += 1;
+	}
+
+	return (len);
+}
+
+/**
+ * main - Prints the first 98 Fibonacci numbers followed by a new line.
  * Return: 0
  */
 int main(void)
 {
-	int f1 = 1;
-	int f2 = 2;
-	int f;
-	int i;
+	unsigned long f1, f2, tmp, max, m, n, t;
+	short int i, init;
 
-	printf("%d, %d ", f1, f2);
+	f1 = 1;
+	f2 = 2;
+	max = 100000000;
+	m = 0;
+	n = 0;
+	t = 0;
+	i = 1;
 
-	for (i = 3; i < 98; i++)
+	for (; i <= 98; i++)
 	{
-		f = f1 + f2;
-		printf("%d", f);
+		if (m > 0)
+			printf("%lu", m);
 
-		if (i < 98)
-			printf(", ");
+		init = numLen(max) - 1 - numLen(f1);
+
+		while (m > 0 && init > 0)
+		{
+			printf("%i", 0);
+			init--;
+		}
+		printf("%lu", f1);
+
+		tmp = (f1 + f2) % max;
+		t = m + n + (f1 + f2) / max;
 		f1 = f2;
-		f2 = f;
-	}
+		m = n;
+		f2 = tmp;
+		n = t;
 
-	putchar('\n');
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
 
 	return (0);
 }
